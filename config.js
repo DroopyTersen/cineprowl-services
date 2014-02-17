d9f2d57130643f0bb6c4b18e919145e596e57d90
Andrew Petersen <andrew.petersen15@gmail.com>
2014-02-16 19:29:56 -0600

    made home slider full width

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	port: 80,
+	mongo: {
+		url: "mongodb://andrew:rival5sof@ds041157.mongolab.com:41157/MongoLab-xd"
+	},
 	movieDb: {
 		key: "9bc8fa1df47f3dde957bbd7f9dd5b48a"
 	},
