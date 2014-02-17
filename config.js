533a39f87218c4bc5870e76a51d6bfac1c8f26a8
Andrew Petersen <andrew.petersen15@gmail.com>
2014-02-16 19:09:02 -0600

    changed service config

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 module.exports = {
-	mongo: {
-		url: "mongodb://cineprowl.com:27017/cineprowl"
+	port: 80,
+	movieDb: {
+		key: "9bc8fa1df47f3dde957bbd7f9dd5b48a"
+	},
+	vlc: {
+		url: "http://runwatcher.com:5050"
+	},
+	nas: {
+		url: "https://runwatcher.com/manage/shares/Movies/"
 	}
 };
