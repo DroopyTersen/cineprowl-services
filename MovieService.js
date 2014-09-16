c9812a9440cd8110ffdf737ebea05dba2712d02d
andrew-petersen <andrew.petersen15@gmail.com>
2014-09-16 18:51:57 +0000

    made models reference case sensitive

--- MovieService.js
+++ MovieService.js
@@ -1,7 +1,7 @@
 "use strict";
 var mongo = require("droopy-mongo"),
 	config = require('./config'),
-	models = require("cineprowl-models"),
+	models = require("CineProwl-Models"),
 	q = require("q"),
 	dao;
 
