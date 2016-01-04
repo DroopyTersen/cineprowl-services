8aa8d2e81a6e7709f59da5059137633a27c87623
Andrew Petersen <Andrew Petersen>
2016-01-04 14:57:59 -0600

    new mongo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: 80,
 	mongo: {
-		url: "mongodb://andrew:rival5sof@ds041157.mongolab.com:41157/MongoLab-xd"
+        url: "mongodb://localhost:27017/cineprowl"
 	},
 	movieDb: {
 		key: "9bc8fa1df47f3dde957bbd7f9dd5b48a"
